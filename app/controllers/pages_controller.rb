84d13bc44a42b72c3f6adab4f04dc98879af99cf
naveed <naveed.sajjad16@gmail.com>
2022-12-15 16:44:26 +0500

    added method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+  
   def about
   end
+
   def unorder
   end
+  
+  private
+  
+  def some_random_method
+    puts "something"
+  end 
 end
